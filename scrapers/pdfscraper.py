575059613c48de2913def836511989d544ae57c4
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-02-04 21:01:13 +0200

    Update pdfscraper.py

--- scrapers/pdfscraper.py
+++ scrapers/pdfscraper.py
@@ -9,3 +9,13 @@ pdf = pdfplumber.open(path)
 # print(pdf.metadata)
 # print(pdf.pages)
 pages = pdf.pages
+
+# pages[1].extract_table()[7][0].replace("\n",'')
+
+string = "This is a sample string"
+
+# Find the index of the substring "sample"
+start_index = string.find('Bachelor')
+
+# Print the portion of the string starting from the index of the substring "sample"
+# print(string[start_index:])
