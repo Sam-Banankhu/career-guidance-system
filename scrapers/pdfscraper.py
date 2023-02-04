2a4d5c73ce91266c46b0fd9372172013b8665302
Samson Banankhu Mhango <sambanankhumhango@gmail.com>
2023-02-04 21:01:50 +0200

    Update pdfscraper.py

--- scrapers/pdfscraper.py
+++ scrapers/pdfscraper.py
@@ -19,3 +19,24 @@ start_index = string.find('Bachelor')
 
 # Print the portion of the string starting from the index of the substring "sample"
 # print(string[start_index:])
+
+pg = []
+for j in range(len(pages)):
+    for i in range(len(pages[j].extract_table())):
+        try:
+            if 'Bachelor' in pages[j].extract_table()[i+1][0].replace("\n",''):
+                course = pages[j].extract_table()[i+1][0].replace("\n",'')
+                # Find the index of the substring "sample"
+                start_index = course.find('Bachelor')
+#                 print(pages[j].extract_table()[i+1][3].replace("\n",''))
+                print(course[start_index:])
+                pg.append(course[start_index:])
+    #             print(pages[j].extract_table()[i+1][9].replace("\n",''))
+
+
+                print()
+            else:
+                continue
+
+        except:
+            continue
